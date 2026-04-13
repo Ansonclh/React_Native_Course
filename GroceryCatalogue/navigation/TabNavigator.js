5990b387162b3b065df263118207824aab72f370
Ansonclh <ansoncheng1116@hotmail.com>
2026-04-13 11:40:52 +0800

    message

--- GroceryCatalogue/navigation/TabNavigator.js
+++ GroceryCatalogue/navigation/TabNavigator.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import { Text,StyleSheet} from 'react-native';
+import { Text,StyleSheet, SafeAreaView} from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import FruitsScreen from '../screens/FruitsScreen';
+import VegetablesScreen from '../screens/VegetablesScreen';
+import DairyScreen from '../screens/DairyScreen';
 
 function TabNavigator() {
+  const Tab = createBottomTabNavigator();
   return (
     <SafeAreaProvider>
-      <Text>Tab Navigator</Text>
+      {/* <Text>Tab Navigator</Text> */}
+      <NavigationContainer>
+        <SafeAreaView style={styles.safeArea}>
+          <Text style={styles.brandName}>FreshMart</Text>
+        </SafeAreaView>
+        <Tab.Navigator>
+          <Tab.Screen name="Fruits" component={FruitsScreen} />
+          <Tab.Screen name="Vegetables" component={VegetablesScreen} options={{headerShown: false,}} />
+          <Tab.Screen name="Dairy" component={DairyScreen} options={{headerShown: false,}} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
     
   )
